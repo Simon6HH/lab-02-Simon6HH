06c2f6783d6807848184f27c63c854476ceaba7b
Simon Hackbert <60202596+Simon6HH@users.noreply.github.com>
2021-02-11 15:46:43 -0600

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class TwoSum {
+    public static int [] TwoSum (int[] num, int sum) { /** Method initializer, two parameters: int array num and int sum**/
+        for (int i = 0; i < num.length - 1; i++) {
+            for (int j = 0; j < num.length; j++) { /** Nested for loop, runs through i, then runs through j, finding two numbers**/
+                if ((num[i] + num[j] == sum)) { /** If the answer to "sum" is here, the if statement will move to a return statement **/
+                    return new int[] {i, j}; /** Return the array of the correct two indexes of the numbers that add to the sum**/
+                }
+            }
+        }
+        return new int[] {-1}; /** Returns -1 if no solution**/
+    }
+
+    public static void main (String[] args) {
+        int[] num = {2, 12, 8, 7}; /** Initializes the array with several integers to test for the correct sum**/
+        int sum = 9;
+        System.out.println(Arrays.toString(TwoSum(num, sum)) + " = " + sum); /** Prints the num array with the correct added numbers **/
+    }
+}
